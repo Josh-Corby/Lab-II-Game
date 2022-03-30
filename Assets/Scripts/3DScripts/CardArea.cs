866d55ce3b0d3bb529e8b68866d45c24a7c634d7
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2022-03-30 17:23:02 +1100

    wednesday week 7

---
+++ Assets/Scripts/3DScripts/CardArea.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardArea : GameBehaviour
+{
+    public bool isOccupied = false;
+    void Start()
+    {
+        isOccupied = false;
+    }
+}
